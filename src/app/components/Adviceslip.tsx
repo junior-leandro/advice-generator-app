71d4074a68ab547b5fba693feca0d87303950ba8
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-11-04 11:23:13 -0300

    Display of text obtained with the API

--- src/app/components/Adviceslip.tsx
+++ src/app/components/Adviceslip.tsx
@@ -5,19 +5,27 @@ import { useEffect, useState } from "react";
 
 const AdviceSlip = () => {
 
-    const[data, setData] = useState({
+    const [data, setData] = useState({
         id: "",
         advice: ""
     });
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://api.adviceslip.com/advice')
-        .then(response => response.json())
-        .then(data => console.log(data))
-    },[])
+            .then((response) => response.json())
+            .then((data) =>
+                setData({
+                    id: data.slip.id,
+                    advice: data.slip.advice,
+                })
+            ).catch((error) => console.error("Error ", error));
+    }, []);
 
-    return(
-        <span className="text-white ">Texto aqui </span>
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <h2 className="text-white"> Adivice: {data.id} </h2>
+            <span className="text-white ">{data.advice}</span>
+        </div>
     )
 }
 
